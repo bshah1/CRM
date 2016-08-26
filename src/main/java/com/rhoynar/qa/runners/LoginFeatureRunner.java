a95a6a0db4b99e55b8830112cd86b22ce494923c
Bhumi Shah <bhumi01@comcast.net>
2016-08-26 09:37:04 -0600

    Commit temp change

--- src/main/java/com/rhoynar/qa/runners/LoginFeatureRunner.java
+++ src/main/java/com/rhoynar/qa/runners/LoginFeatureRunner.java
@@ -4,4 +4,6 @@ package com.rhoynar.qa.runners;
  * Created by Bhumi on 8/26/2016.
  */
 public class LoginFeatureRunner {
+    // This is a login feature runner file. It should be empty...
+    // This is a login feature runner file. It should be empty...
 }
